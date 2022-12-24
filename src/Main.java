90d88e9e9cf10e167170cd2c25bf0773c4caeb81
krut_ <egorrak3221@mail.ru>
2022-12-24 13:46:34 +0300

    cat

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,9 @@ public class Main {
         System.out.println("Hello world!");
         System.out.println("привет мир!");
         System.out.println("привет!");
+
+        var cat = "кошка";
+        System.out.println("cat!");
+
     }
 }
